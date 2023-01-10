af9b7b2e9bb515e033fc22645c2f402db71ca287
peterbrian254 <peterbrian8957@gmail.com>
2023-01-10 04:53:25 -0800

    new

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -1,20 +1,13 @@
 #!/usr/bin/python3
-"""
-read method
+"""append_write module.
+Contains a function that appends a text file.
 """
 
 
-def read_lines(filename="", nb_lines=0):
-    """read n lines of text file
-    args:
-        filename: file to read
-        nb_lines: n line
-    return:
-        na
+def append_write(filename="", text=""):
     """
-    line_num = 0
-    with open(filename, encoding="utf-8") as f:
-        for line in f:
-            if line_num < nb_lines or not nb_lines or nb_lines < 0:
-                print(line, end="")
-            line_num += 1
+    Appends a string at the end of a text file (UTF8)
+    and returns the number of characters added.
+    """
+    with open(filename, 'a') as f:
+        return f.write(text)
