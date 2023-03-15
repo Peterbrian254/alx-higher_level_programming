a0b4128979edcb47c3fb52ef9929ffca43d0cb3c
peterbrian254 <peterbrian8957@gmail.com>
2023-03-15 13:10:00 -0700

    new

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,7 +1,8 @@
 #!/usr/bin/node
+// prints the addition of 2 integers
+
 function add (a, b) {
   return parseInt(a) + parseInt(b);
 }
-const num1 = process.argv[2];
-const num2 = process.argv[3];
-console.log(add(num1, num2));
+
+console.log(add(process.argv[2], process.argv[3]));
