7bdd609945383778c55b4ec6fdaf959b78eb5250
peterbrian254 <peterbrian8957@gmail.com>
2023-03-15 13:04:25 -0700

    new

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,8 +1,13 @@
 #!/usr/bin/node
-const newArgs = process.argv.slice(2);
-if (isNaN(newArgs[0]) === false) {
-  const n = parseInt(newArgs[0]);
-  for (let i = 0; i < n; i++) { console.log('C is fun'); }
-} else {
+// prints 3 lines: (like 1-multi_languages.js) but by
+// using an array of string and a loop
+
+const lang = 'C is fun';
+
+if (isNaN(process.argv[2])) {
   console.log('Missing number of occurrences');
+} else {
+  for (let i = 0; i < parseInt(process.argv[2]); i++) {
+    console.log(lang);
+  }
 }
